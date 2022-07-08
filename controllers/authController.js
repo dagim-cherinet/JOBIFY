39396c72a8cd4c63794a28dc49d789edbcc4d9e9
Dagim Cherinet <80765887+dagim-cherinet@users.noreply.github.com>
2022-07-08 10:46:34 +0300

    added error handling mechanism

--- controllers/authController.js
+++ controllers/authController.js
@@ -10,6 +10,27 @@ const register = async (req, res) => {
   //   // res.status(500).json({ msg: "something went wrong ...." });
   //   next(err);
   // }
+  class CustomAPIError extends Error {
+    constructor(message) {
+      super(message);
+    }
+  }
+  class BadRequestError extends CustomAPIError {
+    constructor(message) {
+      super(message);
+      this.statusCode = StatusCodes.BAD_REQUEST;
+    }
+  }
+  class NotFoundError extends CustomAPIError {
+    constructor(message) {
+      super(message);
+      this.statusCode = StatusCodes.NOT_FOUND;
+    }
+  }
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    throw new BadRequestError("please provide all values");
+  }
   const user = await User.create(req.body);
   console.log(req.body);
   res.status(StatusCodes.CREATED).json({ user });
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,11 +1,24 @@
 import { StatusCodes } from "http-status-codes";
 const errorHandler = (err, req, res, next) => {
-  console.log(err);
+  console.log(err.message);
   const defaultError = {
-    statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
-    msg: "something went wrong",
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || "something went wrong",
   };
-  res.status(defaultError.statusCode).json({ msg: err });
+  if (err.name === "ValidationError") {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    // defaultError.msg = err.message;
+    //iterate through the errors object ---> Object.values turn the object values into an array
+    defaultError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(", ");
+  }
+  if (err.code && err.code === 11000) {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    defaultError.msg = `${Object.keys(err.keyValue)} has to be unique`;
+  }
+  // res.status(defaultError.statusCode).json({ msg: err });
+  res.status(defaultError.statusCode).json({ msg: defaultError.msg });
 };
 
 export default errorHandler;
